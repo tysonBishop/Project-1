a0f806e1d2d9c5222dea4119bef20fe9c2838871
Tyson Bishop <33107644+tysonBishop@users.noreply.github.com>
2019-04-01 14:15:08 -0700

    Add files via upload

--- reporting.py
+++ reporting.py
@@ -1,5 +1,4 @@
 import psycopg2
-import argparse
 
 
 def query_db(query):
@@ -13,6 +12,7 @@ def query_db(query):
     db.close()
 
     return data
+    print results
 
 
 def most_read_articles():
@@ -20,7 +20,7 @@ def most_read_articles():
 
     print("Most Popular articles:")
     for article, views in articles:
-        print(u"%s — %d views" % (article, views))
+    print(u"%s — %d views" % (article, views))
 
 
 def most_popular_authors():
@@ -28,7 +28,7 @@ def most_popular_authors():
 
     print("\nMost Popular Authors:")
     for author, views in authors:
-        print(u"%s — %d views" % (author, views))
+    print(u"%s — %d views" % (author, views))
 
 
 def most_error_days():
@@ -36,40 +36,7 @@ def most_error_days():
 
     print("\nDays where more than 1% of total requests resulted in errors")
     for day, percentage in days:
-            print(u"%s — %.1f%%" % (day, percentage))
+    print(u"%s — %.1f%%" % (day, percentage))
 
 
-def analyze():
-    parser = argparse.ArgumentParser(
-        description="Data analysis from the log database."
-    )
-    parser.add_argument(
-        "-Most_Read_Articles",
-        help="Display articles with the most read on top.",
-        action='store_true',
-    )
-    parser.add_argument(
-        "-Most_Read_Authors",
-        help="Display popular authors, those with the"
-             "most article reads on top.",
-        action='store_true',
-    )
-    parser.add_argument(
-        "-errors",
-        help="Display dates where more than 1%% of requests were errors.",
-        action='store_true',
-    )
 
-    args = parser.parse_args()
-    if args.reads:
-        most_read_articles()
-    if args.authors:
-        most_popular_authors()
-    if args.errors:
-        most_error_days()
-    else:
-        parser.error("Please specify an argument :/")
-
-
-if __name__ == "__main__":
-    analyze()
