2f1edb61def52963a4d76e8266050e377070395c
Tyson Bishop <33107644+tysonBishop@users.noreply.github.com>
2019-04-01 14:30:18 -0700

    Add files via upload

--- reporting.py
+++ reporting.py
@@ -12,12 +12,11 @@ def query_db(query):
     db.close()
 
     return data
-    print (results)
+    print(results)
 
 
 def most_read_articles():
-        articles = query_db("SELECT * FROM most_read;")
-
+    articles = query_db("SELECT * FROM most_read;")
     print("Most Popular articles:")
     for article, views in articles:
     print(u"%s — %d views" % (article, views))
@@ -25,18 +24,13 @@ def most_read_articles():
 
 def most_popular_authors():
     authors = query_db("SELECT * FROM most_popular;")
-
     print("\nMost Popular Authors:")
     for author, views in authors:
     print(u"%s — %d views" % (author, views))
 
 
 def most_error_days():
-        days = query_db("SELECT * FROM most_error_days;")
-
+    days = query_db("SELECT * FROM most_error_days;")
     print("\nDays where more than 1% of total requests resulted in errors")
     for day, percentage in days:
     print(u"%s — %.1f%%" % (day, percentage))
-
-
-
